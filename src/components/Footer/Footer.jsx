5a2d06e14e3cd359dc4cda170a4c519eaaa2bf5a
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-17 14:59:03 +0000

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import footer from './../Footer/Footer.css'
+
+const Footer = () => {
+    return (
+           
+       <div className="container">
+       <div className="row">
+         <div className="col-md-12">
+         <div className="footer">
+         
+         <div className="col1"> 
+            <ul>
+           <li><div className="row1">VISIT US IN KENYA</div><br></br>
+            <div className="row2">Nairobi,Kenya</div><br></br>
+            <div className="row3">Belgravia,14 riverside drive,
+              <br></br> 4th floor,off riverside drive ,
+               hanover centre,<br></br>14 riverside drive,
+               6th floor, <br></br>off riverside drive.</div> 
+          </li>
+            </ul>
+            <ul> 
+               <li><div className="row1">VISIT US IN GHANA</div><br></br>
+                <div className="row2"> Accra,Ghana</div>  <br></br>
+                  <div className="row3"> One Airport Square,Airport city 
+               8th floor</div>
+               </li>
+      </ul>
+        <ul> 
+      
+           <li><div className="row1">VISIT US IN NIGERIA</div> <br></br>
+           <div className="row2">Lagos,Nigeria</div> <br></br>
+           <div className="row3"> Sterling Bank Building</div>
+           </li>
+         </ul>
+         </div>
+           
+          
+
+       </div>   
+       </div>  
+       </div>  
+       </div>  
+        
+         
+       
+             
+             
+              
+             
+ 
+    );
+}
+
+export default Footer;
